38a56bfdda8b92c4edcb0756e536a56d1aff5a50
stephenkore <30597283+stephenkore@users.noreply.github.com>
2021-06-01 13:41:55 +0530

    Add files via upload

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import { Link } from "react-router-dom";
 
+const Header = () =>{
+    return (       
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
+            <div className="container">
+            <Link className="navbar-brand" to="#">Navbar</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                 <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                    </li>                                 
+                </ul>
+            </div>
+            </div>
+        </nav>        
+        )
+    }
+
+    export default Header;
